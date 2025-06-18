e34455816fc847aee8d42015cab5551baee71e30
Paul Niemczyk <mrpndev@gmail.com>
2025-06-18 18:29:07 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Welcome to your front end</h1>
+    </>
+  )
+}
+
+export default App
