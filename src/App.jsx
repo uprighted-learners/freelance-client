5823a19004d4f93b473cf0dfb0395b47956b96fe
Gabe Vitagliano <gabevitagliano@gmail.com>
2025-07-07 19:23:07 -0400

    auth-frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Auth from './components/Auth'
+
 
 function App() {
+  
+	const [sessionToken, setSessionToken ] = useState(undefined)
 
-  return (
-    <>
-     <h1>Welcome to your front end</h1>
-    </>
-  )
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"))
+    }
+  },[])
+
+  const updateLocalStorage = newToken => {
+    localStorage.setItem("token", newToken)
+    setSessionToken(newToken)
+  }
+
+
+
+	const renderView = () => {
+		return !sessionToken
+		? <Auth updateLocalStorage={updateLocalStorage} />
+		: <Rooms sessionToken={sessionToken} />
+	
+	}
+
+	const logout = () => {
+		if (localStorage.getItem("token")) {
+			localStorage.removeItem("token")
+			setSessionToken(undefined)
+		}
+	}
+
+	return (
+		<>
+			<button onClick={logout}>Logout</button>
+			{renderView()}
+		</>
+	)
+	
 }
 
 export default App
